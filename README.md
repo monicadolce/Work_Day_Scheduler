603503bf43a942aec3c7796c7eaeddd186a00561
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-07-23 10:41:35 -0400

    updated README with demo gif and url links

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Work_Day_Scheduler
 Work Day Scheduler is a calendar application that allows a user to save events for each hour of the day.
+
+<img src="./assets/images/05-third-party-apis-homework-demo.gif" alt="Work-day-scheduler" />
+
+
+Application deployed at live URL : <a href= "https://monicadolce.github.io/Work_Day_Scheduler/">Visit Live Javascript Quiz</a>
+
+Application GitHub URL : <a href= "https://github.com/monicadolce/Work_Day_Scheduler">Visit Monica's GitHub Repository</a> 
---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// Display current date using Moment.js
+var today = moment().format("MMM DD, YYYY");
+$("#currentDay").text(today);
+
+// 
+var container = $(".container").click(function (event) {
+
+    if (event.target.nodeName === "I") {
+
+        var buttonId = event.target.attributes[0].nodeValue;
+        var inputValue = $("." + buttonId).val()
+        localStorage.setItem(buttonId, inputValue);
+    }
+})
+
+// 
+$(".9am").val(localStorage.getItem("9am"));
+$(".10am").val(localStorage.getItem("10am"));
+$(".11am").val(localStorage.getItem("11am"));
+$(".12pm").val(localStorage.getItem("12pm"));
+$(".1pm").val(localStorage.getItem("1pm"));
+$(".2pm").val(localStorage.getItem("2pm"));
+$(".3pm").val(localStorage.getItem("3pm"));
+$(".4pm").val(localStorage.getItem("4pm"));
+$(".5pm").val(localStorage.getItem("5pm"));
+
+// 
+$(".entry-item").each(function (event) {
+    console.log(this);
+    var blockTime = $(this).data("time")
+    console.log(blockTime);
+    var currentTime = moment().format("HH");
+    console.log(currentTime);
+    if (currentTime > blockTime) {
+        $(this).addClass("past")
+    } else if (currentTime == blockTime) {
+        $(this).addClass("present")
+    } else {
+
+        $(this).addClass("future")
+    }
+})
+
+
+

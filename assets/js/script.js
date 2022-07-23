7ed1cdb4260ea489f6c45d1ed46ea795135c7445
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-07-23 13:40:34 -0400

    additional comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Display current date using Moment.js
+// Display current date using Moment.js.
 var today = moment().format("MMM DD, YYYY");
 $("#currentDay").text(today);
 
-// The setItem() method using key name and value updates local storage
+// The setItem() method using key name and value updates local storage.
 var container = $(".container").click(function (event) {
 
     if (event.target.nodeName === "I") {
@@ -13,7 +13,7 @@ var container = $(".container").click(function (event) {
     }
 })
 
-// The getItem() method returns key value in local storage
+// The getItem() method returns key value in local storage.
 $(".9am").val(localStorage.getItem("9am"));
 $(".10am").val(localStorage.getItem("10am"));
 $(".11am").val(localStorage.getItem("11am"));
@@ -24,14 +24,18 @@ $(".3pm").val(localStorage.getItem("3pm"));
 $(".4pm").val(localStorage.getItem("4pm"));
 $(".5pm").val(localStorage.getItem("5pm"));
 
-// 
+// The each() method allows to loop through elements in the JQuery collection; 'this' is a keyword and refers to the element.
 $(".entry-item").each(function (event) {
     console.log(this);
+// The data() method attaches data to DOM elements.
     var blockTime = $(this).data("time")
     console.log(blockTime);
+// Moment.js formats the hour into 24-hour format.
     var currentTime = moment().format("HH");
     console.log(currentTime);
+// If and else statement to display color coded time blocks
     if (currentTime > blockTime) {
+// The addClass() method adds class names to the selected elements.
         $(this).addClass("past")
     } else if (currentTime == blockTime) {
         $(this).addClass("present")
